20f5d3e4e9ededa5a228fb273c7c73f8390ee711
Nasr Galal <nasrbue@gmail.com>
2022-01-25 20:59:42 +0200

    test: add todo cases

--- components/atomic/TestBtn/TestBtn.spec.ts
+++ components/atomic/TestBtn/TestBtn.spec.ts
@@ -29,4 +29,38 @@ describe('TestBtn', () => {
     const btn = wrapper.find('[data-test="btn"]')
     expect(btn.text()).toBe('Jest default title')
   });
+
+  it.todo('should render an outlined btn')
+  it.todo('should render an icon btn')
+  it.todo('should render a depressed btn')
+  it.todo('should render a plain btn')
+  it.todo('should render a rounded btn')
+  it.todo('should render a fab btn')
+  it.todo('should render a text btn')
+  it.todo('should render a block btn')
+  it.todo('should render a small btn')
+  it.todo('should render a x-small btn')
+  it.todo('should render a large btn')
+  it.todo('should render a x-large btn')
+  it.todo('should render a disabled btn')
+  it.todo('should render a loading btn')
+  it.todo('should handle click events')
+
+  /**
+   * :depressed="depressed"
+    :icon="icon"
+    :outlined="outlined"
+    :plain="plain"
+    :rounded="rounded"
+    :fab="fab"
+    :text="text"
+    :block="block"
+    :disabled="disabled"
+    :large="large"
+    :loading="loading"
+    :small="small"
+    :x-large="xLarge"
+    :x-small="xSmall"
+    v-on:click="$emit('click', $event)"
+   */
 })
